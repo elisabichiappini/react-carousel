01dbc84bd286b301b7f9cfbf441cb850acdb7fef
elisabichiappini <elisabichiappini@gmail.com>
2026-07-01 16:46:29 +0200

    FEAT: new post

--- src/components/CarouselStyle.jsx
+++ src/components/CarouselStyle.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-icons/io";
 
 export default function CarouselStyle() {
-    const [currentIndex, setCurrentIndex] = useState(2);
+    const [currentIndex, setCurrentIndex] = useState(0);
     const currentPost = posts[currentIndex];
 
     const goToNext = () => setCurrentIndex(c => (c + 1) % posts.length);
@@ -14,25 +14,31 @@ export default function CarouselStyle() {
 
     return (
 
-
-        <div className="carousel">
-            <div class="cards-wrapper">
-                <div className="cards" style={{     marginLeft: `calc(-${currentIndex * 640}px + 1rem + 20px)` }}>
-                    {
-                        posts.map((post, index) => (
-                            <div key={`card-${index}`} className={`card ${index === currentIndex ? 'active' : ''}`}>
-                                <figure>
-                                    <img src={post.img} alt={post.title} />
-                                </figure>
-                                <p>{post.title}</p>
-                            </div>
-                        ))
-                    }
+        <div className="carousel-wrapper">
+            <div className="carousel">
+                <div class="cards-wrapper">
+                    <div className="cards" style={{ marginLeft: `calc(-${currentIndex * 640}px + 1rem + 20px)` }}>
+                        {
+                            posts.map((post, index) => (
+                                <div key={`card-${index}`} className={`card ${index === currentIndex ? 'active' : ''}`}>
+                                    <figure>
+                                        <img src={post.img} alt={post.title} />
+                                    </figure>
+                                    <p>{post.title}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
+                <button className="arrow left" onClick={goToPrev}><LeftArrow /></button>
+                <button className="arrow right" onClick={goToNext}><RightArrow /></button>
             </div>
-            <button className="arrow left" onClick={goToPrev}><LeftArrow /></button>
-            <button className="arrow right" onClick={goToNext}><RightArrow /></button>
+            <div className="bullets">
+                ciao
+            </div>
+
         </div>
+
 
     )
 }
